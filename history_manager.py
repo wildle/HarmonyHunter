00d66f33f34cfa66ec9dfdf30d94db6614c9fd71
Lenard <le.wild@mci4me.at>
2024-02-25 09:23:33 +0100

    Add timestamp to history entries

--- history_manager.py
+++ history_manager.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from tinydb import TinyDB, Query
 
 class HistoryManager:
@@ -6,7 +7,8 @@ class HistoryManager:
         self.history_table = self.db.table('history')
 
     def add_to_history(self, song_id, title):
-        self.history_table.insert({'song_id': song_id, 'title': title})
+        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        self.history_table.insert({'time': current_time, 'title': title})
 
     def get_history(self):
         return self.history_table.all()
